ae461f52e909e2f0d7689ea7e448987f4db57c5c
mgagah <mifgagah@gmail.com>
2019-07-11 11:30:19 +0800

    Add files via upload

---
+++ hexbiner.py
@@ -0,0 +1,21 @@
+a=5
+b=5
+iint=int(a) + int(b)
+_bol=bool(a) + bool(b)
+_chr=chr(a) + chr(b)
+_str=str(a) + str(b)
+_bin=bin(a) + bin(b)
+_hex=hex(a) + hex(b)
+_oct=oct(a) + oct(b)
+
+ffloat=float(a) +float(b)
+
+print ("integer",iint)
+print ("floatnya",ffloat)
+
+print("bool",_bol)
+print("chr",_chr)
+print("str",_str)
+print("bin",_bin)
+print("hex",_hex)
+print("oct",_oct)
